4b04592c46b33f846f879de0140854656739e9e7
Tapan Shah <tapans@outlook.com>
2015-10-12 03:20:29 -0400

    several bug fixes and enhancements

--- app.js
+++ app.js
@@ -62,8 +62,7 @@ function getDirections(mode, origin, destination, callback){
 			return console.error(err);	
 		} else {
 			body = JSON.parse(body);
-			if (body.status.toLowerCase() !== "ok") return callback(body.status + ": " + params);
-			console.log(body);
+			if (body.status.toLowerCase() !== "ok") return callback(body.status + ": " + params);			
 
 			var mssg = generateMssg(mode, body);
 			return callback(mssg);
@@ -82,16 +81,15 @@ function generateMssg(mode, data){
 
 	var mssg = printSummary(startAddress, endAddress, totalDistance, totalDuration, mode);
 	var stepNum = 1;
-	switch(mode){
+
+	switch(mode){		
 		case "transit":
 			var arrivalTime = routeData.arrival_time.text;
 			var departureTime = routeData.departure_time.text;			
 			mssg += "" + departureTime + " - " + arrivalTime + "\n";
 			for (var j = 0; j < numSteps; j++){
 				var step = steps[j];		
-				mssg += printStep(stepNum, step.html_instructions, step.distance.text, step.duration.text);
-				stepNum++;	
-				switch(step.travel_mode.toLowerCase){
+				switch(step.travel_mode.toLowerCase()){
 					case "walking":
 						var walkingSteps = step.steps;
 						var numWalkingSteps = walkingSteps.length;
@@ -104,20 +102,21 @@ function generateMssg(mode, data){
 					case "transit":						
 						var summary = step.html_instructions;
 						var transitDetails = step.transit_details;
-
 						var arrivalStopName = transitDetails.arrival_stop.name;
 						var arrivalTime = transitDetails.arrival_time.text;
 						var departureStopName = transitDetails.departure_stop.name;
 						var departureTime = transitDetails.departure_time.text;
 						var agency = transitDetails.line.agencies[0].name;
 						var numStops = transitDetails.num_stops;
+						var distance = step.distance.text;
+						var duration = step.duration.text;
 
-						mssg += printTransitStep(stepNum, agency, departureStopName, departureTime, arrivalStopName, arrivalTime, numStops, summary);
-						numSteps++;
+						mssg += printTransitStep(stepNum, agency, departureStopName, departureTime, arrivalStopName, arrivalTime, numStops, summary, distance, duration);
+						stepNum++;
 						break;
 					case "default":
 						mssg += printStep(stepNum, step.html_instructions, step.distance.text, step.duration.text);	
-						numSteps++;					
+						stepNum++;					
 						break;						
 				}
 			}
@@ -134,17 +133,21 @@ function generateMssg(mode, data){
 	return mssg;
 }
 
-function stripHtml(string){
+function capitalize(string){
+	return string[0].toUpperCase() + string.slice(1);
+}
 
+function stripHtml(string){
 	return string.replace(/<(?:.|\n)*?>/gm, '');
 }
 
 function printSummary(startAddress, endAddress, distance, duration, mode){
 	mode = mode.toLowerCase();
-	if (mode !== "transit" || mode !== "driving" || mode !== "bicycling" || mode !== "walking"){
+	if (mode !== "transit" && mode !== "driving" && mode !== "bicycling" && mode !== "walking"){
 		mode = "driving";	
 	}
-	var line = mode + " Directions from " + startAddress + " to " + endAddress + ". \n(" + distance + " in " + duration + ")" + "\n";
+	mode = capitalize(mode);
+	var line = mode + " directions from " + startAddress + " to " + endAddress + ". \n(" + distance + " in " + duration + ")" + "\n";
 	return line;
 }
 
@@ -154,8 +157,8 @@ function printStep(stepNum, stepMssg, stepDistance, stepDuration){
 	return line;
 }
 
-function printTransitStep(stepNum, agency, departureStopName, departureTime, arrivalStopName, arrivalTime, numStops, summary){
+function printTransitStep(stepNum, agency, departureStopName, departureTime, arrivalStopName, arrivalTime, numStops, summary, distance, duration){
 	var line = stepNum + ". " + agency + ": " + summary + " (" + numStops + " stops). ";
-	line += "Depart " + departureStopName + " at" + departureTime + " and arrive at " + arrivalStopName + " at " + arrivalTime + "\n";
+	line += "Depart " + departureStopName + " at " + departureTime + " and arrive at " + arrivalStopName + " at " + arrivalTime + ". (" + distance + " in " + duration + ")" + "\n";
 	return line;
 }
